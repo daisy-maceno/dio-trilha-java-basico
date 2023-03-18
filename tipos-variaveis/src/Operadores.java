a95bf6c101bcf1026c2868c21f33321f29966fbf
Daísy Maceno <daisymaceno12@gmail.com>
2023-03-18 19:44:34 -0300

    Update Operadores.java

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,31 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.print(nomeCompleto);
+        String nomeUm = "Daisy";
+        String nomeDois = new String("Daisy");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+
+        int numero1 = 1;
+        int numero2 = 2;   
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 == numero2){ //true
+            System.out.println("a nossa condição é verdadeira");
+        }
+        System.out.println("numeroUm é igaul a numeroDois? " + simNao);
+        
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior a numeroDois? " + simNao);
+
+        
 
     }
     
